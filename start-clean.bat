f70f64abd851daa50a2ec452ff06dd95b3db2d8f
Merry Apple Edano <ctu.rrya@gmail.com>
2026-03-25 22:27:52 +0800

    Remove leftover md/sql/bat clutter files

--- start-clean.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Clearing Expo and Metro cache...
-if exist .expo rmdir /s /q .expo
-if exist node_modules\.cache rmdir /s /q node_modules\.cache
-echo Cache cleared!
-echo.
-echo Starting Expo with clean cache...
-npx expo start --clear
